eac6d0d78a68dedfee04099bcbe33469ea6ce300
Ferencz <dev@localhost>
2025-02-28 22:33:10 +0300

    git ignore for tests and test images

---
+++ tests/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "============="
+npm run test:rescale
+echo "==================================="
+npm run test:rescale3
+echo "==================================="
+npm run test:grayscale
+echo "=========================================="
+npm run test:compare
+echo "======================="
+npm run test:comparef
+echo "==========================="
